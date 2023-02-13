475c6614247b91c2263b90a230ab13d3db36d02b
Patel <hp256908@gmail.com>
2023-02-13 21:00:56 +0530

    svg not visible

--- lib/views/home/widgets/home_screen_drawer.dart
+++ lib/views/home/widgets/home_screen_drawer.dart
@@ -1,26 +1,51 @@
 import 'package:cpm/core/constants/pallets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreenDrawer extends StatelessWidget {
   const HomeScreenDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Pallets.primaryColor,
-            ),
-            child: IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.menu),
-            ),
-          ),
+    return Drawer(child:ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+SizedBox(height: 50,),
+        ListTile(
+    title: Text('Projects',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+        ),
+        ListTile(
+          title: Text('Group',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+        ), ListTile(
+    title: Text('Students',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+    ), ListTile(
+    title: Text('Faculties',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+    ), ListTile(
+    title: Text('Branches',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+    ),ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget>[
+          Text('List 1'),
+          Text('List 2'),
+          Text('List 3'),
         ],
-      ),
-    );
+      ), ListTile(
+    title: Text('Technolgies',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+
+    ), ListTile(
+    title: Text('Academics',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+
+    ),
+
+
+      ]));
+
+
+
   }
-}
+  }
