16c326bed7fcba29b488e389efa2a27bb8287744
Jayesh Mardiya <mardiyajayesh@gmail.com>
2023-02-14 22:21:13 +0530

    created basic drawer

--- lib/views/home/widgets/home_screen_drawer.dart
+++ lib/views/home/widgets/home_screen_drawer.dart
@@ -7,19 +7,213 @@ class HomeScreenDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Pallets.primaryColor,
+      child: SafeArea(
+        bottom: false,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // Projects
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Projects',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
             ),
-            child: IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.menu),
+            // Groups
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Groups',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
             ),
-          ),
-        ],
+            // Students
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Students',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+            // Faculties
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Faculties',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+            // Branches
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Branches',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+            // Technologies
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Technologies',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+            // Academics
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Academics',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+            // Categories
+            ListTile(
+              leading: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Pallets.secondaryColor,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/projects_4.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ),
+              ),
+              title: const Text(
+                'Categories',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Pallets.primaryColor,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
