839db4f9d848dd6a2d8a28d401c926f04c452933
Jayesh Mardiya <mardiyajayesh@gmail.com>
2023-02-14 23:09:39 +0530

    called api for home screen with error of null sub type

--- lib/views/home/controllers/home_screen_controller.dart
+++ lib/views/home/controllers/home_screen_controller.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
+import 'package:projectify/views/home/models/home_screen_model.dart';
+import 'package:projectify/views/home/providers/home_screen_provider.dart';
 
 class HomeScreenController extends GetxController {
   RxBool isNotifications = false.obs;
 
   TextEditingController searchController = TextEditingController();
+
+  Rx<HomeScreenModel?> homeScreenModel = null.obs;
+
+  @override
+  void onInit() async {
+    var data = await HomeScreenProvider().fetch(null);
+    homeScreenModel.value = data;
+    super.onInit();
+  }
 }
